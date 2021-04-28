ca2b1c3d6d44258b96b6d661b11cf88e7ffd46e9
Nikas <solarisocean@gmail.com>
2021-04-28 15:40:14 +0300

    changed to Sydney

--- src/lt/niko/Main.java
+++ src/lt/niko/Main.java
@@ -6,7 +6,7 @@ public class Main {
         GeoLocation someLocation = new GeoLocation();
         GeoLocation vilnius = new GeoLocation(54.683333, 25.283333);
         GeoLocation kaunas = new GeoLocation(54.897222, 23.886111);
-        GeoLocation hamilton = new GeoLocation(42.827014, -75.544624);
+        GeoLocation sydney = new GeoLocation(-33.865143, 151.209900);
         GeoLocation nearVilnius = new GeoLocation(vilnius);
 
         someLocation.print();
@@ -16,7 +16,7 @@ public class Main {
         System.out.println("Number of locations: " + GeoLocation.getNumLocations());
 
         System.out.println("From Vilnius to Kaunas: " + GeoLocation.distance(vilnius, kaunas));
-        System.out.println("From Vilnius to Hamilton: " + GeoLocation.distance(vilnius, hamilton));
+        System.out.println("From Vilnius to Sydney: " + GeoLocation.distance(vilnius, sydney));
         System.out.println("From Vilnius to random location: " + GeoLocation.distance(vilnius, someLocation));
         System.out.println("From Vilnius to random neighbour: " + GeoLocation.distance(vilnius, nearVilnius));
 
